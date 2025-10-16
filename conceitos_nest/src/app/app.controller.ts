1e20ee663ccae88c20bca1e1d04e15bff7ba5084
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2025-10-16 15:43:41 -0300

    🐈‍⬛ 8 - Básico sobre protocolo HTTP, URLs, recursos e métodos de solicitação (GET)

--- conceitos_nest/src/app/app.controller.ts
+++ conceitos_nest/src/app/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
 
-@Controller()
+@Controller('home') // /home
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // Método da solicitação => Read
+  // /home/hello
+  @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return 'Qualquer coisa';
   }
 }
