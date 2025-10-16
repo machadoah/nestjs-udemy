1773bf9bf59941b7d2bfa299fdbceffb1e4ccfbb
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2025-10-16 16:02:47 -0300

    🐈‍⬛ 10 - Continuação da aula anterior + Extensão Rest Client do VS Code

--- conceitos_nest/src/app/app.controller.ts
+++ conceitos_nest/src/app/app.controller.ts
@@ -12,4 +12,9 @@ export class AppController {
     // return this.appService.getHello();
     return 'Qualquer coisa';
   }
+
+  @Get('exemplo')
+  exemplo() {
+    return 'Exemplo de rota';
+  }
 }
