6638bffdec231e7a174d4335f2a06943e8505d59
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2025-10-17 16:11:14 -0300

    🐈‍⬛ 33 - Configurando o TypeOrmModule com PostgreSQL (módulo do TypeORM para NestJS)

--- crud_nest_typeorm/src/app.module.ts
+++ crud_nest_typeorm/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemosModule } from './memos/memos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MemosModule],
+  imports: [
+    MemosModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5400,
+      username: 'user',
+      database: 'memos',
+      password: 'password',
+      autoLoadEntities: true, // loads entities without having to specify them
+      synchronize: true, // 🚨 synchronizes actions with the database (for development only)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
