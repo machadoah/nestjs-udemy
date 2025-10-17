c0a2fac7425352e86412987d06d6d8682520c4b1
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2025-10-17 16:27:37 -0300

    🐈‍⬛ 34 - Primeira Entity do TypeOrm no NestJS (primeira tabela na base de dados PSQL)

--- crud_nest_typeorm/src/memos/entities/memo.entity.ts
+++ crud_nest_typeorm/src/memos/entities/memo.entity.ts
@@ -1,8 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('memos')
 export class MemoEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({
+    length: 255,
+  })
   text: string;
+
+  @Column({
+    length: 50,
+  })
+  @Column({
+    length: 50,
+  })
   from: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+  })
   to: string;
+
+  @Column({ default: false })
   wasRead: boolean;
+
+  @Column()
   date: Date;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
--- crud_nest_typeorm/src/memos/memos.module.ts
+++ crud_nest_typeorm/src/memos/memos.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MemosController } from './memos.controller';
 import { MemosService } from './memos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MemoEntity } from './entities/memo.entity';
 
 // nest generate module recados
 
 @Module({
   controllers: [MemosController],
   providers: [MemosService],
+  imports: [TypeOrmModule.forFeature([MemoEntity])],
 })
 export class MemosModule {}
