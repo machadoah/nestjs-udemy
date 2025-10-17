d0a3be4936b321c65da8ba8ec13fbe5e19a20c44
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2025-10-17 12:13:55 -0300

    🐈‍⬛ 30 - Segurança - opções whitelist e forbidNonWhitelisted de ValidationPipe do NestJS

--- crud_nest/src/main.ts
+++ crud_nest/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove keys that are not in the DTO
+      forbidNonWhitelisted: true, // throws error when the key does not exist
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
